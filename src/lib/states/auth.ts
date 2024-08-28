20fad3dbe68c043b39676228d6f39bcdb85a958c
Sparsh Kaushik <contact@Sparshkaushik.com>
2024-08-28 14:18:44 +0530

    [Persist Auth Store] - Enabled Auth Store Persist to LocalStorage

--- src/lib/states/auth.ts
+++ src/lib/states/auth.ts
@@ -1,7 +1,26 @@
 import { observable } from "@legendapp/state";
+import { enableReactTracking } from "@legendapp/state/config/enableReactTracking";
+import { ObservablePersistLocalStorage } from "@legendapp/state/persist-plugins/local-storage";
+import { configureObservableSync, syncObservable } from "@legendapp/state/sync";
+
+enableReactTracking({
+  auto: true,
+});
+
+configureObservableSync({
+  persist: {
+    plugin: ObservablePersistLocalStorage,
+  },
+});
 
 export type UserAuth = {
   accessToken: string;
 };
 
 export const auth$ = observable<UserAuth | null>(null);
+
+syncObservable(auth$, {
+  persist: {
+    name: "auth",
+  },  
+});
